5b34adc01e907737a735e2911c78fefb9c3f2056
Iman Aziz <84653267+Iman-IT@users.noreply.github.com>
2023-10-02 06:20:25 +0500

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
           <div className="col-12 col-md-6 col-sm-12 mb-4">
             <p className="pt-5">Let's Introduce</p>
             <h1 className=''>Country Heads Meeting Tracker</h1>
-            <a href="" id='btn' className='px-2 py-2 mt-5 justify-content-center align-items-center'> Details</a>
+            <a href="" id='btn' className='px-2 py-2 mt-5 justify-content-center align-items-center'> Lets Start</a>
           </div>
           <div className="col-12 col-md-6 col-sm-12">
             <img src="2.jpg" className="img-fluid" alt="Meeting Tracker" id="main" />
