fc2bd705ddf329e699d016191f8634ebf0e931f6
Iman Aziz <84653267+Iman-IT@users.noreply.github.com>
2023-10-03 08:06:09 +0500

    MeetingDetails.jsx

--- src/components/MeetingDetails.jsx
+++ src/components/MeetingDetails.jsx
@@ -14,9 +14,10 @@ function MeetingDetail({ data }) {
   return (
     <Wrapper className="contianer">
       <div className="row mx-auto d-flex justify-content-center align-items-center">
-<div className="col-12 col-md-6 col-sm-4 mt-3">
+<div className="col-12 col-md-6 col-sm-4 mt-3" id='border'>
  
     <div className='card shadow-lg px-5 py-5'>
+      <h4>Meeting Title: </h4>
             <h2>{meeting.meetingName}</h2>
             <h8>Description: {meeting.description}</h8>
             <div className="row mt-3">
@@ -43,6 +44,9 @@ function MeetingDetail({ data }) {
 export default MeetingDetail;
 
 const Wrapper = styled.div`
+#border{
+  border: 2px solid #2C3B53;
+}
 h5{
   color: #2C3B53;
 }
